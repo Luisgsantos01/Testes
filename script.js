dd6821d7b74259450ef5bccbbbfa502087301a87
Luisgsantos01 <luisgsantos01@gmail.com>
2025-05-14 22:19:03 -0300

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function sim() {
-    alert("Você não é um supervisor, você é um pai! <3");
+    alert("Agora você não pode mais escapar! Vou te mandar o itinerário completo, incluindo paradas para café e selfies obrigatórias! :3");
 }
 
 function desvia(btn) {
